fd1933f9b31f90cf05a46b5616e9659a45ec600d
AdnanHafeez <adnanhr81@gmail.com>
2018-11-22 16:27:26 +1100

    Added code for the server

--- server/app.js
+++ server/app.js
@@ -7,3 +7,20 @@ const errorHandler = require('errorhandler');
 const mongoose = require('mongoose');
 
 mongoose.promise = global.Promise;
+const isProduction = process.env.NODE_ENV === 'production';
+
+const app = express();
+
+app.use(cors());
+app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname,'public')));
+app.use(session({secret:'LightBlog', cookie:{maxAge: 60000}, resave:false, saveUninitialized:false}));
+
+(if(!isProduction)) {
+  app.use(errorHandler());
+}
+
+mongoose.connect('mongodb://localhost/lightblog');
+mongoose.set('debug',true);
